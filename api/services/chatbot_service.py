7d7f07e62cec58931c3e703a1cd2876afac8fe1e
Daniel-Cas <dmattcomics@gmail.com>
2022-10-23 11:59:00 -0500

    Add default response

--- api/services/chatbot_service.py
+++ api/services/chatbot_service.py
@@ -3,4 +3,4 @@ from fastapi import WebSocket
 
 async def processor_response(webSocket: WebSocket):
     message = await webSocket.receive_json()
-    return f"Your message is: {message}"
+    return f"Hola, ¿que tal? Lamento decirte esto, pero aún no estoy disponible :c"
