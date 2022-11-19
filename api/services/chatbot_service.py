7a9d19a6e77de8be66ca4b4bb1efa45eb99ddb4f
Daniel-Cas <dmattcomics@gmail.com>
2022-11-19 12:10:12 -0500

    Finish integration with dialogflow

--- api/services/chatbot_service.py
+++ api/services/chatbot_service.py
@@ -1,9 +1,23 @@
 from fastapi import WebSocket
-from time import sleep
+import os
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'private_key.json'
+DIALOGFLOW_PROJECT_ID = 'nice-theater-367918'
+DIALOGFLOW_LANGUAGE_CODE = 'es'
+SESSION_ID = 'me'
+
+session_client = dialogflow.SessionsClient()
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 
 
 async def processor_response(webSocket: WebSocket):
     message = await webSocket.receive_json()
-    sleep(1)
-    return f"Hola, ¿que tal? Lamento decirte esto, pero aún no estoy disponible :c"
+    text_input = dialogflow.types.TextInput(text=message['message'], language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+        return response.query_result.fulfillment_text
+    except InvalidArgument:
+        raise
